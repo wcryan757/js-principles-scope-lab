9940e54c0019e8ab94e86878442a5ed81ef84368
William Ryan <wcryan757@gmail.com>
2021-04-06 20:55:34 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+
+var customerName = "bob"
+var bestCustomer 
+const leastFavoriteCustomer = "nolan"
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+    
+function setBestCustomer() {
+        bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "bob"
+}
